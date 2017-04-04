e7132eb3b0fa6c2fff1f2f2680f480be40429946
Smith, Tim <Tim.Smith@inin.com>
2017-04-04 09:43:19 -0600

    adding publish script

---
+++ resources/sdk/purecloudruby/scripts/publishGem.sh
@@ -0,0 +1,30 @@
+BUILD_DIR=$1
+GEM_NAME=$2
+GEM_KEY=$3
+IS_NEW_RELEASE=$4
+VERSION=$5
+
+echo "BUILD_DIR=$BUILD_DIR"
+echo "GEM_NAME=$GEM_NAME"
+echo "IS_NEW_RELEASE=$IS_NEW_RELEASE"
+echo "VERSION=$VERSION"
+
+# Why?
+#export PATH=$PATH:/home/jenkins/bin
+
+GEM_CREDENTIALS_FILE=~/.gem/credentials
+GEM_KEY_NAME="developer_evangelists"
+
+cd $BUILD_DIR
+
+# Ensure credentials are available
+if grep -Fq $GEM_KEY_NAME $GEM_CREDENTIALS_FILE
+then
+    echo "Found API key"
+else
+	echo "Adding API key"
+    echo "\n:$GEM_KEY_NAME: $GEM_KEY" >> $GEM_CREDENTIALS_FILE
+fi
+
+# Publish gem
+gem push $GEM_NAME-$VERSION.gem --key $GEM_KEY_NAME
