772adeb5f72fff5afea044c71fbdddcd80a2f704
Smith, Tim <Tim.Smith@inin.com>
2017-04-07 15:02:13 -0600

    optional deploy

--- resources/sdk/purecloudpython/scripts/publishPyPi.sh
+++ resources/sdk/purecloudpython/scripts/publishPyPi.sh
@@ -1,3 +1,15 @@
+IS_NEW_RELEASE=$7
+VERSION=$8
+
+echo "IS_NEW_RELEASE=$IS_NEW_RELEASE"
+echo "VERSION=$VERSION"
+
+if [ ! "$IS_NEW_RELEASE" = "true" ]
+then
+	echo "Skipping PyPi deploy"
+	exit 0
+fi
+
 # Publish egg on PyPi
 python setup.py register -r pypi-sdk
 python setup.py sdist upload -r pypi-sdk
