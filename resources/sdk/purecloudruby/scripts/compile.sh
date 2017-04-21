a420f9f6dc32d5f261f6a1efeeb60c67b620ede3
Smith, Tim <Tim.Smith@inin.com>
2017-04-21 10:00:16 -0600

    updating script

--- resources/sdk/purecloudruby/scripts/compile.sh
+++ resources/sdk/purecloudruby/scripts/compile.sh
@@ -1,8 +1,10 @@
 BUILD_DIR=$1
 EXTENSIONS_TEMP=$2
+VERSION=$3
 
 echo "BUILD_DIR=$BUILD_DIR"
 echo "EXTENSIONS_TEMP=$EXTENSIONS_TEMP"
+echo "VERSION=$VERSION"
 
 # Add auth extension
 cat $BUILD_DIR/lib/purecloudplatformclientv2.rb $EXTENSIONS_TEMP/auth.rb > $EXTENSIONS_TEMP/purecloudplatformclientv2.tmp
@@ -13,5 +15,5 @@ cd $BUILD_DIR
 gem build purecloudplatformclientv2.gemspec
 
 #test
-gem install $BUILD_DIR/purecloudplatformclientv2*
-ruby resources/sdk/purecloudruby/tests/test.rb
+gem install $BUILD_DIR/purecloudplatformclientv2-$VERSION.gem --no-ri --no-rdoc
+ruby $COMMON_ROOT/resources/sdk/purecloudruby/tests/test.rb
