514ee74c9c9e340889c461f686718a2b4d495685
Smith, Tim <Tim.Smith@inin.com>
2017-06-06 10:35:04 -0600

    add browserify/minify output

--- resources/sdk/purecloudjavascript/scripts/compile.sh
+++ resources/sdk/purecloudjavascript/scripts/compile.sh
@@ -1,8 +1,23 @@
 SDK_REPO=$1
 BUILD_DIR=$2
 RESOURCE_DIR=$3
+PACKAGE_NAME=$4
 
 echo "SDK_REPO=$SDK_REPO"
 echo "BUILD_DIR=$BUILD_DIR"
 echo "RESOURCE_DIR=$RESOURCE_DIR"
+echo "PACKAGE_NAME=$PACKAGE_NAME"
 
+exit 0
+
+cd $BUILD_DIR
+mkdir web
+
+npm install browserify -g
+npm install uglify-es -g
+
+echo "Browserifying..."
+browserify "$BUILD_DIR/src/$PACKAGE_NAME/index.js" > "$BUILD_DIR/web/$PACKAGE_NAME.js"
+
+echo "Minifying...."
+uglifyjs "$BUILD_DIR/web/$PACKAGE_NAME.js" --compress --output "$BUILD_DIR/web/$PACKAGE_NAME.min.js"
