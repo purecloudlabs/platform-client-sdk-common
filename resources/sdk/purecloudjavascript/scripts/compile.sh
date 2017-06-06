321205652ff5d6e96ca3be54807947ea50a06499
Smith, Tim <Tim.Smith@inin.com>
2017-06-06 14:20:20 -0600

    removing models and cleanup for browserify

--- resources/sdk/purecloudjavascript/scripts/compile.sh
+++ resources/sdk/purecloudjavascript/scripts/compile.sh
@@ -8,16 +8,18 @@ echo "BUILD_DIR=$BUILD_DIR"
 echo "RESOURCE_DIR=$RESOURCE_DIR"
 echo "PACKAGE_NAME=$PACKAGE_NAME"
 
-exit 0
+#exit 0
 
 cd $BUILD_DIR
 mkdir web
 
-npm install browserify -g
-npm install uglify-es -g
+#npm install browserify -g
+#npm install uglify-es -g
 
 echo "Browserifying..."
-browserify "$BUILD_DIR/src/$PACKAGE_NAME/index.js" > "$BUILD_DIR/web/$PACKAGE_NAME.js"
+browserify -r "$BUILD_DIR/src/$PACKAGE_NAME/index.js:platformClient" "$BUILD_DIR/src/$PACKAGE_NAME/index.js" > "$BUILD_DIR/web/$PACKAGE_NAME.js"
 
 echo "Minifying...."
 uglifyjs "$BUILD_DIR/web/$PACKAGE_NAME.js" --compress --output "$BUILD_DIR/web/$PACKAGE_NAME.min.js"
+
+
