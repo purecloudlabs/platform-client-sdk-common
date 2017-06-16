0611514aacaa7a9d40eab3e7ac32b3544b07a02f
Smith, Tim <Tim.Smith@inin.com>
2017-06-16 15:08:27 -0600

    API-2449 js sdk publish s3

---
+++ resources/sdk/purecloudjavascript/scripts/publishS3.sh
@@ -0,0 +1,25 @@
+BUILD_DIR=$1
+IS_NEW_RELEASE=$2
+VERSION=$3
+
+echo "BUILD_DIR=$BUILD_DIR"
+echo "IS_NEW_RELEASE=$IS_NEW_RELEASE"
+echo "VERSION=$VERSION"
+
+
+if [ ! "$IS_NEW_RELEASE" = "true" ]
+then
+	echo "Skipping npm deploy"
+	exit 0
+fi
+
+cd $BUILD_DIR
+
+aws s3 cp \
+	$BUILD_DIR/web \
+	s3://inin-index-files-prod/developercenter-cdn/javascript/$SDK_VERSION \
+	--recursive --acl "public-read"
+aws s3 cp \
+	$BUILD_DIR/web \
+	s3://inin-index-files-prod/developercenter-cdn/javascript/latest \
+	--recursive --acl "public-read"
