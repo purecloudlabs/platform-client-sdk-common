65a95632737494ab61241f9c91e28dbf66d2f030
Smith, Tim <Tim.Smith@inin.com>
2017-06-16 14:30:11 -0600

    API-2449 js sdk prod npm publish

---
+++ resources/sdk/purecloudjavascript/scripts/publishNpm.sh
@@ -0,0 +1,18 @@
+NPM_AUTH_TOKEN=$1
+BUILD_DIR=$2
+IS_NEW_RELEASE=$3
+
+echo "BUILD_DIR=$BUILD_DIR"
+echo "IS_NEW_RELEASE=$IS_NEW_RELEASE"
+
+
+if [ ! "$IS_NEW_RELEASE" = "true" ]
+then
+	echo "Skipping npm deploy"
+	exit 0
+fi
+
+echo "ca=null
+//registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > ./.npmrc
+
+npm publish
