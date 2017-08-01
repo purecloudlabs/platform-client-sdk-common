38aefeb133fa547a0be4b8f73d685f49e662ee22
Tim Smith <tim.j.smith@genesys.com>
2017-08-01 11:06:00 -0600

    API-2672 clean up npm package

--- resources/sdk/purecloudjavascript/scripts/publishNpm.sh
+++ resources/sdk/purecloudjavascript/scripts/publishNpm.sh
@@ -1,8 +1,10 @@
 NPM_AUTH_TOKEN=$1
 BUILD_DIR=$2
-IS_NEW_RELEASE=$3
+TEMP_DIR=$3
+IS_NEW_RELEASE=$4
 
 echo "BUILD_DIR=$BUILD_DIR"
+echo "TEMP_DIR=$TEMP_DIR"
 echo "IS_NEW_RELEASE=$IS_NEW_RELEASE"
 
 
@@ -12,7 +14,15 @@ then
 	exit 0
 fi
 
-cd $BUILD_DIR
+PACKAGE_DIR=$TEMP_DIR/nodepackage
+
+mkdir $PACKAGE_DIR
+mkdir $PACKAGE_DIR/src
+
+cp $BUILD_DIR/package.json $PACKAGE_DIR/package.json
+cp -r $BUILD_DIR/src $PACKAGE_DIR/src
+
+cd $PACKAGE_DIR
 
 echo "ca=null
 //registry.npmjs.org/:_authToken=${NPM_AUTH_TOKEN}" > ./.npmrc
