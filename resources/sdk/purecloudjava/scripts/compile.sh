3ca283318c87d3616662a7ff07b57cbe92c5f96d
Tim Smith <tim.j.smith@genesys.com>
2018-03-08 08:45:05 -0700

    API-3187 Preventing deploy target when no new version

--- resources/sdk/purecloudjava/scripts/compile.sh
+++ resources/sdk/purecloudjava/scripts/compile.sh
@@ -2,10 +2,12 @@ BUILD_MODE=$1 # package/verify/deploy/deploy:deploy
 BUILD_DIR=$2
 MAVEN_SETTINGS_FILE=$3
 DPGP_PASSPHRASE=$4
+IS_NEW_RELEASE=$5
 
 echo "BUILD_MODE=$BUILD_MODE"
 echo "BUILD_DIR=$BUILD_DIR"
 echo "MAVEN_SETTINGS_FILE=$MAVEN_SETTINGS_FILE"
+echo "IS_NEW_RELEASE=$IS_NEW_RELEASE"
 
 # Verify settings
 if [ ! -z "$MAVEN_SETTINGS_FILE" ]
@@ -22,6 +24,12 @@ if [ ! "$BUILD_MODE" = "package" ] && [ ! "$BUILD_MODE" = "verify" ] && [ ! "$BU
 then
 	echo "Unknown build mode $BUILD_MODE"
 	exit 1
+fi
+
+if [ "$BUILD_MODE" = "deploy" ] && [ ! "$IS_NEW_RELEASE" = "true" ]
+then
+	echo "IS_NEW_RELEASE=$IS_NEW_RELEASE and BUILD_MODE=$BUILD_MODE, forcing BUILD_MODE to verify"
+	BUILD_MODE="verify"
 fi
 
 # Add maven to PATH
