4245c7deeddcae698149e1738b8e58309f1bb5c9
Tim Smith <tim.j.smith@genesys.com>
2018-03-13 14:57:52 -0600

    API-3235 Newtonsoft.json 9.0.1 -> 10.0.2

--- resources/sdk/pureclouddotnet/scripts/compile.sh
+++ resources/sdk/pureclouddotnet/scripts/compile.sh
@@ -16,7 +16,7 @@ mozroots --import --sync
 mono $COMMON_DIR/resources/sdk/pureclouddotnet/bin/nuget.exe install $BUILD_DIR/src/$ROOT_NAMESPACE/packages.config -o $BUILD_DIR/packages -NoCache -Verbosity detailed;
 mkdir -p $BUILD_DIR/bin;
 
-cp $BUILD_DIR/packages/Newtonsoft.Json.9.0.1/lib/net45/Newtonsoft.Json.dll $BUILD_DIR/bin/Newtonsoft.Json.dll;
+cp $BUILD_DIR/packages/Newtonsoft.Json.10.0.2/lib/net45/Newtonsoft.Json.dll $BUILD_DIR/bin/Newtonsoft.Json.dll;
 cp $BUILD_DIR/packages/RestSharp.105.2.3/lib/net45/RestSharp.dll $BUILD_DIR/bin/RestSharp.dll;
 cp $BUILD_DIR/packages/WebSocketSharp.1.0.3-rc11/lib/websocket-sharp.dll $BUILD_DIR/bin/websocket-sharp.dll;
 
