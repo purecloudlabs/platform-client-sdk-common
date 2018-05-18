ddf75ef174b44484fffcfa159fd5c7cc3c3967e6
Tim Smith <tim.j.smith@genesys.com>
2018-05-18 11:33:54 -0600

    API-3234 rollup config cleanup + build scripts

--- resources/sdk/purecloudjavascript/scripts/compile.sh
+++ resources/sdk/purecloudjavascript/scripts/compile.sh
@@ -12,22 +12,34 @@ echo "PACKAGE_NAME=$PACKAGE_NAME"
 
 cd $BUILD_DIR
 
+echo "Installing npm global modules..."
+npm i -g rollup@0.59.1
+npm i -g uglify-es@3.3.9
+npm i -g browserify@16.2.2
+
+echo "Installing dependencies..."
 npm i
-npm run rollup || { echo "rollup failed"; exit 1; }
 
-cd $BUILD_DIR
-mkdir web
+# Build cjs/node
+echo "Executing rollup (cjs/node)..."
+rollup -c rollup-cjs-for-node.config.js || { echo "Rollup (cjs/node) failed"; exit 1; }
 
-# https://github.com/substack/node-browserify
-npm install browserify -g
-
-# https://github.com/mishoo/UglifyJS2
-npm install uglify-es -g
+# Build cjs/browserify
+echo "Executing rollup (cjs/browserify)..."
+rollup -c rollup-cjs-for-browserify.config.js || { echo "Rollup (cjs/browserify) failed"; exit 1; }
 
 echo "Browserifying..."
-browserify -r "$BUILD_DIR/bundle.js:${MODULE_NAME}" > "$BUILD_DIR/web/$PACKAGE_NAME.js" || { echo "Browserify failed"; exit 1; }
-
-# Each file
+browserify -r "$BUILD_DIR/dist/web-cjs/bundle.js:${MODULE_NAME}" > "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.js" || { echo "Browserify failed"; exit 1; }
 
 echo "Minifying...."
-uglifyjs "$BUILD_DIR/web/$PACKAGE_NAME.js" --compress --mangle --output "$BUILD_DIR/web/$PACKAGE_NAME.min.js" || { echo "Minify failed"; exit 1; }
+uglifyjs "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.js" --compress --mangle --output "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.min.js" || { echo "Minify failed (cjs/browserify)"; exit 1; }
+
+# Build amd
+echo "Executing rollup (amd)..."
+rollup -c rollup-amd.config.js || { echo "Rollup (amd) failed"; exit 1; }
+
+echo "Minifying...."
+uglifyjs "$BUILD_DIR/dist/web-amd/$PACKAGE_NAME.js" --compress --mangle --output "$BUILD_DIR/dist/web-amd/$PACKAGE_NAME.min.js" || { echo "Minify failed (amd)"; exit 1; }
+
+
+echo "Compile script complete"
