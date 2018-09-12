14b3f7b5c8be4a9608e3f4d154d409a2457e5e80
Tim Smith <tim.j.smith@genesys.com>
2018-09-12 13:02:55 -0600

    API-2176 Remove alternate iOS SDK templates

--- sdkBuilder.js
+++ sdkBuilder.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const swaggerDiff = require('./modules/swaggerDiff');
 const Builder = require('./modules/builder');
-const sdkLanguageRegex = /^(purecloudjava|purecloudjavascript|pureclouddotnet|purecloudruby|purecloudpython|purecloudios)$/i;
+const sdkLanguageRegex = /^(purecloudjava|purecloudjavascript|pureclouddotnet|purecloudruby|purecloudpython|purecloudios|purecloudswift4)$/i;
 
 try {
 	// Parse language regex for hoomans
