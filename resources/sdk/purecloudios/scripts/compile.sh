e192ba0d485c6da898eaafe2a172cd108c8a4afd
Tim Smith <tim.j.smith@genesys.com>
2019-08-06 13:29:42 -0600

    API-3730 adding ios docs

--- resources/sdk/purecloudios/scripts/compile.sh
+++ resources/sdk/purecloudios/scripts/compile.sh
@@ -6,10 +6,7 @@ echo "SDK_REPO=$SDK_REPO"
 echo "BUILD_DIR=$BUILD_DIR"
 echo "RESOURCE_DIR=$RESOURCE_DIR"
 
-
-
 echo "Beep. Boop. Look at me! I'm a computer compiling things!"
 
-
-mkdir "$BUILD_DIR/docs"
+# Copy readme as doc index file
 cp "$BUILD_DIR/README.md" "$BUILD_DIR/docs/index.md"
