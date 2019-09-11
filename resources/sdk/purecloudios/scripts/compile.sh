aed421d05138c786639977921b50d7327e0132b7
Tim Smith <tim.j.smith@genesys.com>
2019-09-11 10:48:30 -0600

    no-jira use dynamic package name

--- resources/sdk/purecloudios/scripts/compile.sh
+++ resources/sdk/purecloudios/scripts/compile.sh
@@ -1,10 +1,12 @@
 SDK_REPO=$1
 BUILD_DIR=$2
 RESOURCE_DIR=$3
+PACKAGE_NAME=$4
 
 echo "SDK_REPO=$SDK_REPO"
 echo "BUILD_DIR=$BUILD_DIR"
 echo "RESOURCE_DIR=$RESOURCE_DIR"
+echo "PACKAGE_NAME"=$PACKAGE_NAME
 
 echo "Beep. Boop. Look at me! I'm a computer compiling things!"
 
@@ -12,4 +14,4 @@ echo "Beep. Boop. Look at me! I'm a computer compiling things!"
 cp "$BUILD_DIR/README.md" "$BUILD_DIR/docs/index.md"
 
 # Copy podspec to root; cocoapods can't handle subdirectories
-cp "$BUILD_DIR/PureCloudPlatformClientV2.podspec" "$SDK_REPO/PureCloudPlatformClientV2.podspec"
+cp "$BUILD_DIR/$PACKAGE_NAME.podspec" "$SDK_REPO/$PACKAGE_NAME.podspec"
