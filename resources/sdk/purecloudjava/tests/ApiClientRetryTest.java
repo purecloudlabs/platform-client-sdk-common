f55e86602a4ad11465d8fc7480f23f5e748711eb
Deerghayu Shrestha <deerghayu.shrestha@genesys.com>
2020-02-26 19:23:28 -0500

    API-4076 - add elapsed time for each tests

--- resources/sdk/purecloudjava/tests/ApiClientRetryTest.java
+++ resources/sdk/purecloudjava/tests/ApiClientRetryTest.java
@@ -6,7 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+import com.google.common.base.Stopwatch;
+import java.util.concurrent.TimeUnit;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -61,8 +62,11 @@ public class ApiClientRetryTest {
         retryConfiguration.setMaxRetryTime(30);
 
         ApiClient.Retry retry = new ApiClient.Retry(retryConfiguration);
+        Stopwatch stopwatch = Stopwatch.createStarted();
         boolean result = retry.shouldRetry(response);
         Assert.assertEquals(false, result, "Status Code is 200, so shouldRetry method returns false");
+        Assert.assertTrue(stopwatch.elapsed(TimeUnit.MILLISECONDS) < 100);
+        stopwatch.stop();
     }
 
     @Test(priority = 2)
@@ -111,8 +115,12 @@ public class ApiClientRetryTest {
         retryConfiguration.setMaxRetryTime(30);
 
         ApiClient.Retry retry = new ApiClient.Retry(retryConfiguration);
+        Stopwatch stopwatch = Stopwatch.createStarted();
         boolean result = retry.shouldRetry(response);
         Assert.assertEquals(true, result, "Status Code is 429, so it will sleep for 1 Sec as provided in Retry-After header and returns true");
+        Assert.assertTrue(stopwatch.elapsed(TimeUnit.MILLISECONDS) < 1100);
+        stopwatch.stop();
+
     }
 
     @Test(priority = 3)
@@ -161,8 +169,11 @@ public class ApiClientRetryTest {
         retryConfiguration.setMaxRetryTime(30);
 
         ApiClient.Retry retry = new ApiClient.Retry(retryConfiguration);
+        Stopwatch stopwatch = Stopwatch.createStarted();
         boolean result = retry.shouldRetry(response);
         Assert.assertEquals(true, result, "Status Code is 502, so it will sleep for 1 Sec as provided in Retry-After header and returns true");
+        Assert.assertTrue(stopwatch.elapsed(TimeUnit.MILLISECONDS) < 1100);
+        stopwatch.stop();
     }
 
     @Test(priority = 4)
@@ -208,10 +219,12 @@ public class ApiClientRetryTest {
         };
 
         ApiClient.RetryConfiguration retryConfiguration = new ApiClient.RetryConfiguration();
-
+        Stopwatch stopwatch = Stopwatch.createStarted();
         ApiClient.Retry retry = new ApiClient.Retry(retryConfiguration);
         boolean result = retry.shouldRetry(response);
         Assert.assertEquals(false, result, "Even though Status Code is 502, it will return false because MaxRetryTime is set to Zero by default");
+        Assert.assertTrue(stopwatch.elapsed(TimeUnit.MILLISECONDS) < 100);
+        stopwatch.stop();
     }
 
     @Test(priority = 5)
@@ -263,8 +276,11 @@ public class ApiClientRetryTest {
         retryConfiguration.setMaxRetriesBeforeBackoff(0);
 
         ApiClient.Retry retry = new ApiClient.Retry(retryConfiguration);
+        Stopwatch stopwatch = Stopwatch.createStarted();
         boolean result = retry.shouldRetry(response);
         Assert.assertEquals(true, result, "Since Status Code is 503 and maxRetriesBeforeBackoff is Zero, backoff block will be executed and returns true");
+        Assert.assertTrue(stopwatch.elapsed(TimeUnit.MILLISECONDS) < 3100);
+        stopwatch.stop();
     }
 
     @Test(priority = 6)
@@ -311,8 +327,11 @@ public class ApiClientRetryTest {
         ApiClient.RetryConfiguration retryConfiguration = new ApiClient.RetryConfiguration();
         retryConfiguration.setMaxRetryTime(30);
         ApiClient.Retry retry = new ApiClient.Retry(retryConfiguration);
+        Stopwatch stopwatch = Stopwatch.createStarted();
         boolean result = retry.shouldRetry(response);
         Assert.assertEquals(true, result, "Even though Retry-After header is missing, it will sleep for 3 Sec by default and returns true");
+        Assert.assertTrue(stopwatch.elapsed(TimeUnit.MILLISECONDS) < 3100);
+        stopwatch.stop();
     }
 
 }
