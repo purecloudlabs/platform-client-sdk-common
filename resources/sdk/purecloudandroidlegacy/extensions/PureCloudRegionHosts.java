1d7aaae1aecf874997edd8e0860e778a0d077050
Tim Smith <tim.j.smith@genesys.com>
2020-03-23 10:50:31 -0600

    API-4421 adding initial config

---
+++ resources/sdk/purecloudandroidlegacy/extensions/PureCloudRegionHosts.java
@@ -0,0 +1,23 @@
+package com.mypurecloud.sdk.v2;
+
+public enum PureCloudRegionHosts {
+    us_east_1( "https://api.mypurecloud.com"),
+    eu_west_1("https://api.mypurecloud.ie"),
+    ap_southeast_2( "https://api.mypurecloud.com.au"),
+    ap_northeast_1("https://api.mypurecloud.jp"),
+    eu_central_1("https://api.mypurecloud.de"),
+    us_west_2("https://api.usw2.pure.cloud"),
+    ca_central_1("https://api.cac1.pure.cloud"),
+    ap_northeast_2("https://api.apne2.pure.cloud"),
+    eu_west_2("https://api.euw2.pure.cloud");
+
+
+    private String apiHost;
+    PureCloudRegionHosts(String apiHost) {
+        this.apiHost = apiHost;
+    }
+
+    public String getApiHost() {
+        return apiHost;
+    }
+}
