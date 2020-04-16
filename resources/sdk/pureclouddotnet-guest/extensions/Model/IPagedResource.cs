5cb3dea22bf1ef8a1c560228f05147e455bd03ec
Ronan Watkins <ronan.watkins@genesys.com>
2020-04-16 16:37:55 +0100

    API-4358 Implementing DotNet Guest Chat SDK

---
+++ resources/sdk/pureclouddotnet-guest/extensions/Model/IPagedResource.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using {{=it.packageName}}.Model;
+
+namespace {{=it.packageName }}.Model
+{
+	public interface IPagedResource<T>
+	{
+        int? PageSize { get; set; }
+        int? PageNumber { get; set; }
+        long? Total { get; set; }
+        string SelfUri { get; set; }
+        string FirstUri { get; set; }
+        string PreviousUri { get; set; }
+        string NextUri { get; set; }
+        string LastUri { get; set; }
+        int? PageCount { get; set; }
+        List<T> Entities { get; set; }
+	}
+}
---
+++ resources/sdk/pureclouddotnet-guest/scripts/compile.sh
@@ -0,0 +1,39 @@
+BUILD_DIR=$1
+COMMON_DIR=$2
+ROOT_NAMESPACE=$3
+
+echo "BUILD_DIR=$BUILD_DIR"
+echo "COMMON_DIR=$COMMON_DIR"
+echo "ROOT_NAMESPACE=$ROOT_NAMESPACE"
+
+# CD to build dir
+cd $BUILD_DIR
+
+# Import certs
+if test -f "/etc/pki/tls/certs/ca-bundle.crt"; then
+	echo "Using cert-sync"
+	cert-sync /etc/pki/tls/certs/ca-bundle.crt
+else
+	echo "Using mozroots"
+	mozroots --import --sync
+fi
+
+
+# Install packages
+echo "Installing packages"
+mono $COMMON_DIR/resources/sdk/pureclouddotnet-guest/bin/nuget.exe install $BUILD_DIR/src/$ROOT_NAMESPACE/packages.config -o $BUILD_DIR/packages -NoCache -Verbosity detailed;
+
+mkdir -p $BUILD_DIR/bin;
+
+cp $BUILD_DIR/packages/Newtonsoft.Json.11.0.2/lib/net45/Newtonsoft.Json.dll $BUILD_DIR/bin/Newtonsoft.Json.dll;
+cp $BUILD_DIR/packages/RestSharp.106.3.1/lib/net452/RestSharp.dll $BUILD_DIR/bin/RestSharp.dll;
+
+echo "Compiling SDK..."
+mcs -r:$BUILD_DIR/bin/Newtonsoft.Json.dll,\
+$BUILD_DIR/bin/RestSharp.dll,\
+System.Runtime.Serialization.dll \
+-target:library \
+-out:$BUILD_DIR/bin/${ROOT_NAMESPACE}.dll \
+-doc:$BUILD_DIR/bin/${ROOT_NAMESPACE}.xml \
+-recurse:'src/'${ROOT_NAMESPACE}'/*.cs' \
+-platform:anycpu
