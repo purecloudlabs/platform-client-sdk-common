75347786fbb43088a79bbd6d6de3f75c9b117607
Ronan Watkins <ronan.watkins@genesys.com>
2021-01-19 16:36:15 +0000

    clisdkclient: Re-format API Errors

--- resources/sdk/clisdkclient/extensions/models/httperror.go
+++ resources/sdk/clisdkclient/extensions/models/httperror.go
@@ -1,7 +1,5 @@
 package models
 
-import "fmt"
-
 type HttpStatusError struct {
 	Verb       string
 	Path       string
@@ -11,5 +9,5 @@ type HttpStatusError struct {
 }
 
 func (e HttpStatusError) Error() string {
-	return fmt.Sprintf("HttpStatusCode error encountered while processing request to Genesys Cloud API. Verb %s, Path %s, Status Code %d, Body %v", e.Verb, e.Path, e.StatusCode, e.Body)
+	return e.Body
 }
