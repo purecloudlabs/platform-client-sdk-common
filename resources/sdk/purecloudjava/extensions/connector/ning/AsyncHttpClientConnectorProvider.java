19215f41b65f96a48ed606f79654738f7162c71d
Jacob Shaw <jacob.shaw@genesys.com>
2021-05-21 09:46:08 -0400

    workaround for private access in updated http-client version

--- resources/sdk/purecloudjava/extensions/connector/ning/AsyncHttpClientConnectorProvider.java
+++ resources/sdk/purecloudjava/extensions/connector/ning/AsyncHttpClientConnectorProvider.java
@@ -10,6 +10,8 @@ import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 import org.asynchttpclient.proxy.ProxyServerSelector;
 import org.asynchttpclient.util.ProxyUtils;
+import static org.asynchttpclient.Dsl.proxyServer;
+
 
 import java.io.IOException;
 import java.net.*;
@@ -39,7 +41,7 @@ public class AsyncHttpClientConnectorProvider implements ApiClientConnectorProvi
                 @Override
                 public void connectFailed(URI uri, SocketAddress sa, IOException ioe) { }
             };
-            ProxyServerSelector proxyServerSelector = ProxyUtils.createProxyServerSelector(proxySelector);
+            ProxyServerSelector proxyServerSelector = createProxyServerSelector(proxySelector);
             builder.setProxyServerSelector(proxyServerSelector);
             builder.setUseProxySelector(true);
         }
@@ -48,4 +50,38 @@ public class AsyncHttpClientConnectorProvider implements ApiClientConnectorProvi
         AsyncHttpClient client = new DefaultAsyncHttpClient(config);
         return new AsyncHttpClientConnector(client);
     }
+
+    /*
+     * method source: https://github.com/AsyncHttpClient/async-http-client/blob/9b7298b8f1cb41fed5fb5a1315267be323c875d6/client/src/main/java/org/asynchttpclient/util/ProxyUtils.java
+     */
+    public ProxyServerSelector createProxyServerSelector(final ProxySelector proxySelector) {
+        return uri -> {
+            try {
+              URI javaUri = uri.toJavaNetURI();
+    
+              List<Proxy> proxies = proxySelector.select(javaUri);
+              if (proxies != null) {
+                // Loop through them until we find one that we know how to use
+                for (Proxy proxy : proxies) {
+                  switch (proxy.type()) {
+                    case HTTP:
+                      if (!(proxy.address() instanceof InetSocketAddress)) {
+                        return null;
+                      } else {
+                        InetSocketAddress address = (InetSocketAddress) proxy.address();
+                        return proxyServer(address.getHostString(), address.getPort()).build();
+                      }
+                    case DIRECT:
+                      return null;
+                    default:
+                      break;
+                  }
+                }
+              }
+              return null;
+            } catch (URISyntaxException e) {
+              return null;
+            }
+        };
+      }
 }
