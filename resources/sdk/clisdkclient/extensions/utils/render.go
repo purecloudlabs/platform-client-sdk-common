8e84f648cdd9bdacb6eb9205a2776d3450b4bee0
Michael Roddy <michael.roddy@genesys.com>
2021-08-03 12:48:55 +0100

    adding ability to send and receive data as yaml

--- resources/sdk/clisdkclient/extensions/utils/render.go
+++ resources/sdk/clisdkclient/extensions/utils/render.go
@@ -1,12 +1,30 @@
 package utils
 
 import (
+	"encoding/json"
 	"fmt"
 
+	"github.com/mypurecloud/platform-client-sdk-cli/build/gc/data_format"
 	"github.com/tidwall/pretty"
+	"sigs.k8s.io/yaml"
 )
 
 func Render(data string) {
-	result := pretty.Pretty([]byte(data))
-	fmt.Printf("%s", result)
+	if data_format.OutputFormat == "json" && IsJSON(data) {
+		result := pretty.Pretty([]byte(data))
+		fmt.Printf("%s", result)
+	}
+	if data_format.OutputFormat == "yaml" && IsJSON(data) {
+		result, err := yaml.JSONToYAML([]byte(data))
+		if err != nil {
+			fmt.Printf("err: %v\n\n", err)
+			return
+		}
+		fmt.Println(string(result))
+	}
+}
+
+func IsJSON(str string) bool {
+	var js json.RawMessage
+	return json.Unmarshal([]byte(str), &js) == nil
 }
