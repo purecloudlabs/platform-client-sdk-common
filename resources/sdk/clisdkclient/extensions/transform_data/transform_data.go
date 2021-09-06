6f94bfb38a3fa4149b439fa974c930ccf727e6ea
Michael Roddy <michael.roddy@genesys.com>
2021-09-06 15:42:50 +0100

    changes to README.mustache and transform_data

--- resources/sdk/clisdkclient/extensions/transform_data/transform_data.go
+++ resources/sdk/clisdkclient/extensions/transform_data/transform_data.go
@@ -13,22 +13,31 @@ var (
 	TemplateStr  string
 )
 
-func ConvertJsonToMap(data string) map[string]interface{} {
-	var result map[string]interface{}
-	err := json.Unmarshal([]byte(data), &result)
-	if err != nil {
-		logger.Fatalf("Error unmarshalling JSON %v\n", err)
+func ConvertJsonToMap(data string) interface{} {
+	// if the data is an array of json objects then we need to store that in an array of maps and not just a map
+	if data[0] == '[' {
+		var res []map[string]interface{}
+		err := json.Unmarshal([]byte(data), &res)
+		if err != nil {
+			logger.Fatalf("Error unmarshalling %v\n", err)
+		}
+		return res
 	}
-	return result
+	var res map[string]interface{}
+	err := json.Unmarshal([]byte(data), &res)
+	if err != nil {
+		logger.Fatalf("Error unmarshalling %v\n", err)
+	}
+	return res
 }
 
-func ProcessTemplateFile(mp map[string]interface{}) string {
+func ProcessTemplateFile(mp interface{}) string {
 	path := []string{TemplateFile}
 	tmpl := myMust(template.ParseFiles(path...))
 	return process(tmpl, mp)
 }
 
-func ProcessTemplateStr(mp map[string]interface{}) string {
+func ProcessTemplateStr(mp interface{}) string {
 	tmpl := myMust(template.New("tmpl").Parse(TemplateStr))
 	return process(tmpl, mp)
 }
@@ -37,14 +46,13 @@ func process(t *template.Template, vars interface{}) string {
 	var tmplBytes bytes.Buffer
 	err := t.Execute(&tmplBytes, vars)
 	if err != nil {
-		logger.Fatalf("Error applying parsed template to data object %v\n", err)
+		logger.Fatalf("Error applying parsed template to data object: %v\n", err)
 	}
 	return tmplBytes.String()
 }
 
 // myMust() provides the same functionality as https://cs.opensource.google/go/go/+/refs/tags/go1.17:src/text/template/helper.go;l=23;drc=4f1b0a44cb46f3df28f5ef82e5769ebeac1bc493
-// but does not "panic" and print the stack trace
-// instead it logs and error message and exits the application if the template syntax is not valid
+// but does not "panic" and print the stack trace, instead it logs and error message and exits the application if the template syntax is not valid
 func myMust(t *template.Template, err error) *template.Template {
 	if err != nil {
 		logger.Fatalf("Error invalid syntax in %v\n", err)
