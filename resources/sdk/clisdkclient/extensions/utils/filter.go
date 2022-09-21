fa9a343f0d0ac5611c92d6eca319b880c3cdc04e
Charlie Conneely <charlie.conneely@genesys.com>
2022-09-21 14:10:43 +0100

    Refactored filter.go and refactored + extended tests

--- resources/sdk/clisdkclient/extensions/utils/filter.go
+++ resources/sdk/clisdkclient/extensions/utils/filter.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/mypurecloud/platform-client-sdk-cli/build/gc/logger"
 	"strconv"
 	"strings"
 )
@@ -14,8 +15,26 @@ const (
 	TypeArray          = "array"
 )
 
+const (
+	equalsOperator            string = "=="
+	notEqualsOperator                = "!="
+	lessThanEqualsOperator           = "<="
+	greaterThanEqualsOperator        = ">="
+	lessThanOperator                 = "<"
+	greaterThanOperator              = ">"
+	containsOperator                 = "contains"
+)
+
 var (
-	supportedOperators = []string{"==", "!=", "<=", ">=", "<", ">", "contains"}
+	supportedOperators = []string{
+		equalsOperator,
+		notEqualsOperator,
+		lessThanEqualsOperator,
+		greaterThanEqualsOperator,
+		lessThanOperator,
+		greaterThanOperator,
+		containsOperator,
+	}
 )
 
 func FilterByRegex(data string, expression string) (string, error) {
@@ -27,13 +46,13 @@ func FilterByCondition(data string, condition string) (string, error) {
 	var jsonData interface{}
 	err := json.Unmarshal([]byte(data), &jsonData)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("error unmarshalling json data: %v", err)
 	}
 
 	// Find operator in condition
 	operator := findOperatorInString(condition)
 	if operator == "" {
-		return "", fmt.Errorf("Could not understand condition: %s\n", condition)
+		return "", unrecognizedConditionError(condition)
 	}
 
 	path, value := getFieldKeyAndValueFromConditionString(condition, operator)
@@ -41,7 +60,11 @@ func FilterByCondition(data string, condition string) (string, error) {
 
 	var objects []interface{}
 	if jsonMap, ok := jsonData.(map[string]interface{}); ok {
-		objects = jsonMap["entities"].([]interface{})
+		if entitiesArray, ok := jsonMap["entities"].([]interface{}); ok {
+			objects = entitiesArray
+		} else {
+			return "", entitiesArrayNotFoundError()
+		}
 	} else if jsonArray, ok := jsonData.([]interface{}); ok {
 		objects = jsonArray
 	}
@@ -64,6 +87,8 @@ func findObjectsMatchingCondition(returnedObjects []interface{}, keys []string, 
 	var (
 		allMatchedObjects []interface{}
 		currentObject     interface{}
+		match             bool
+		err               error
 	)
 
 	for _, object := range returnedObjects {
@@ -71,43 +96,51 @@ func findObjectsMatchingCondition(returnedObjects []interface{}, keys []string, 
 		for i, k := range keys {
 			if currentObjectMap, ok := currentObject.(map[string]interface{}); ok {
 				if _, ok := currentObjectMap[k]; !ok {
-					return nil, fmt.Errorf("invalid key '%s' in path", k)
+					logger.Infof("field '%s' not found in current json object", k)
+					break
 				}
 				currentObject = currentObjectMap[k]
 				if _, ok := currentObject.(map[string]interface{}); ok {
 					if i == len(keys)-1 {
-						return nil, fmt.Errorf("key '%s' is type map and is incomparable to value: '%s'", k, value)
+						return nil, incomparableFieldError(k, value)
 					}
 					continue
 				}
 			}
+			if i < len(keys)-1 {
+				return nil, invalidKeyOrderingError(k)
+			}
 			if currentObjectArray, ok := currentObject.([]interface{}); ok {
-				for _, c := range currentObjectArray {
-					match, err := fieldMatchesValue(c, value, "==")
-					if err != nil {
-						return nil, err
-					}
-					if match {
-						allMatchedObjects = append(allMatchedObjects, object.(map[string]interface{}))
-						break
-					}
-				}
+				match, err = fieldMatchesValueInArray(currentObjectArray, value, operator)
 			} else {
-				if i < len(keys)-1 {
-					return nil, fmt.Errorf("the value inside field '%s' is a non-map, but it is not the last key in the provided path", k)
-				}
-				match, err := fieldMatchesValue(currentObject, value, operator)
-				if err != nil {
-					return nil, err
-				}
-				if match {
-					allMatchedObjects = append(allMatchedObjects, object.(map[string]interface{}))
-				}
+				match, err = fieldMatchesValue(currentObject, value, operator)
+			}
+			if err != nil {
+				return nil, err
+			}
+			if match {
+				allMatchedObjects = append(allMatchedObjects, object.(map[string]interface{}))
 			}
 		}
 	}
 
 	return allMatchedObjects, nil
+}
+
+func fieldMatchesValueInArray(jsonArray []interface{}, cliInputValue string, operator string) (bool, error) {
+	if operator != containsOperator {
+		return false, invalidOperatorError(TypeArray, operator)
+	}
+	for _, c := range jsonArray {
+		match, err := fieldMatchesValue(c, cliInputValue, equalsOperator)
+		if err != nil {
+			return false, err
+		}
+		if match {
+			return true, nil
+		}
+	}
+	return false, nil
 }
 
 func fieldMatchesValue(jsonValue interface{}, cliInputValue string, operator string) (bool, error) {
@@ -122,11 +155,11 @@ func fieldMatchesValue(jsonValue interface{}, cliInputValue string, operator str
 
 func compareStrings(jsonValue string, cliInputValue string, operator string) (bool, error) {
 	switch operator {
-	case "==":
+	case equalsOperator:
 		return jsonValue == cliInputValue, nil
-	case "!=":
+	case notEqualsOperator:
 		return jsonValue != cliInputValue, nil
-	case "contains":
+	case containsOperator:
 		return strings.Contains(jsonValue, cliInputValue), nil
 	default:
 		return false, invalidOperatorError(TypeString, operator)
@@ -143,17 +176,17 @@ func compareAsIntegers(jsonValue interface{}, cliInputValue string, operator str
 		return false, err
 	}
 	switch operator {
-	case "==":
+	case equalsOperator:
 		return value1Int == value2Int, nil
-	case "!=":
+	case notEqualsOperator:
 		return value1Int != value2Int, nil
-	case ">":
+	case greaterThanOperator:
 		return value1Int > value2Int, nil
-	case "<":
+	case lessThanOperator:
 		return value1Int < value2Int, nil
-	case ">=":
+	case greaterThanEqualsOperator:
 		return value1Int >= value2Int, nil
-	case "<=":
+	case lessThanEqualsOperator:
 		return value1Int <= value2Int, nil
 	default:
 		return false, invalidOperatorError(TypeInteger, operator)
@@ -162,7 +195,7 @@ func compareAsIntegers(jsonValue interface{}, cliInputValue string, operator str
 
 func compareBooleans(jsonValue bool, cliInputValue string, operator string) (bool, error) {
 	if cliInputValue != "true" && cliInputValue != "false" {
-		return false, fmt.Errorf("'%s' is not boolean. Valid options: true, false", cliInputValue)
+		return false, invalidBooleanValue(cliInputValue)
 	}
 	switch operator {
 	case "==":
@@ -201,4 +234,24 @@ func getKeysFromJsonFieldPath(path string) []string {
 
 func invalidOperatorError(valueType string, operator string) error {
 	return fmt.Errorf("invalid operator for %s comparison: %s\n", valueType, operator)
+}
+
+func entitiesArrayNotFoundError() error {
+	return fmt.Errorf("could not locate entites array inside response object")
+}
+
+func unrecognizedConditionError(condition string) error {
+	return fmt.Errorf("could not understand condition: %s\n", condition)
+}
+
+func incomparableFieldError(key string, value string) error {
+	return fmt.Errorf("key '%s' is incomparable to value: '%s'", key, value)
+}
+
+func invalidKeyOrderingError(key string) error {
+	return fmt.Errorf("the value inside field '%s' is a non-map, but it is not the last key in the provided path", key)
+}
+
+func invalidBooleanValue(value string) error {
+	return fmt.Errorf("'%s' is not boolean. Valid options: true, false", value)
 }
--- resources/sdk/clisdkclient/extensions/utils/filter_test.go
+++ resources/sdk/clisdkclient/extensions/utils/filter_test.go
@@ -7,147 +7,366 @@ import (
 	"testing"
 )
 
-const (
-	trueValue  = "true"
-	falseValue = "false"
-)
-
-const (
-	equalsOperator            string = "=="
-	notEqualsOperator                = "!="
-	lessThanEqualsOperator           = "<="
-	greaterThanEqualsOperator        = ">="
-	lessThanOperator                 = "<"
-	greaterThanOperator              = ">"
-	containsOperator                 = "contains"
-)
-
-var (
-	object1Id           = uuid.NewString()
-	object1Name         = "Charlie C"
-	object1Active       = trueValue
-	object1DivisionName = "Home"
-	object1Version      = "3"
-	object1             = fmt.Sprintf(`
-	{
-		"id": "%s",
-		"name": "%s",
-		"active": %s,
-		"division": {
-			"name": "%s"
-		},
-		"version": %s,
-		"skills": ["Sonic Speed", "Invisibility"]
-	}
-`, object1Id, object1Name, object1Active, object1DivisionName, object1Version)
-
-	object2Id           = uuid.NewString()
-	object2Name         = "Darragh McD"
-	object2Active       = falseValue
-	object2DivisionName = "New"
-	object2Version      = "6"
-	object2             = fmt.Sprintf(`
-	{	
-		"id": "%s",
-		"name": "%s",
-		"active": %s,
-		"division": {
-			"name": "%s"
-		},
-		"version": %s,
-		"skills": ["Flight", "Knitting"]
-	}
-`, object2Id, object2Name, object2Active, object2DivisionName, object2Version)
-
-	jsonTestCaseBasic = fmt.Sprintf(`
-[
-	%s,
-	%s
-]`, object1, object2)
-)
-
-func TestFilterByConditionEquals(t *testing.T) {
-	err := testFilterByConditionWithEqualsOperator(equalsOperator)
-	if err != nil {
-		t.Errorf("ERROR: %v", err)
-	}
-
-	err = testFilterByConditionWithEqualsOperator(notEqualsOperator)
-	if err != nil {
-		t.Errorf("ERROR: %v", err)
-	}
+type OperatorTestCaseStruct struct {
+	condition         string
+	expectedValues    []string
+	notExpectedValues []string
 }
 
-func testFilterByConditionWithEqualsOperator(operator string) error {
+func TestFilterByConditionInvalidCondition(t *testing.T) {
+	type ErrorTestCaseStruct struct {
+		condition     string
+		expectedError error
+	}
+
 	var (
-		condition    string
-		err          error
-		expectedId   string
-		unexpectedId string
+		data = fmt.Sprintf(`
+{
+	"entities": [
+		{
+			"name": "Steve",
+			"age": 21
+		},
+		{
+			"name": "Jake",
+			"age": 32,
+			"dead": false
+		},
+		{
+			"carInfo": {
+				"carReg": "10-D-09"
+			}
+		}
+	]
+}`)
+		testCase1 = ErrorTestCaseStruct{"name=Steve", unrecognizedConditionError("name=Steve")}
+		testCase2 = ErrorTestCaseStruct{"name!Steve", unrecognizedConditionError("name!Steve")}
+		testCase3 = ErrorTestCaseStruct{"name=!Steve", unrecognizedConditionError("name=!Steve")}
+		testCase4 = ErrorTestCaseStruct{"name = Steve", unrecognizedConditionError("name = Steve")}
+		testCase5 = ErrorTestCaseStruct{"name $ Steve", unrecognizedConditionError("name $ Steve")}
+		testCase6 = ErrorTestCaseStruct{"carInfo==foobar", incomparableFieldError("carInfo", "foobar")}
+		testCase7 = ErrorTestCaseStruct{"carInfo.carReg.mileage==100", invalidKeyOrderingError("carReg")}
+		testCase8 = ErrorTestCaseStruct{"dead==untrue", invalidBooleanValue("untrue")}
+		testCases = []ErrorTestCaseStruct{testCase1, testCase2, testCase3, testCase4, testCase5, testCase6, testCase7, testCase8}
 	)
 
-	if operator == equalsOperator {
-		expectedId = object1Id
-		unexpectedId = object2
-	} else if operator == notEqualsOperator {
-		expectedId = object2Id
-		unexpectedId = object1Id
-	} else {
-		return fmt.Errorf("only types %s and %s should be passed to function testFilterByConditionWithEqualsOperator", expectedId, unexpectedId)
+	for _, test := range testCases {
+		err := verifyInvalidOperationError(data, test.condition, test.expectedError)
+		if err != nil {
+			t.Error(err)
+		}
 	}
 
-	// Filter by bool field
-	condition = fmt.Sprintf("active%s%s", operator, object1Active)
-	err = verifyValueReturnedWithCondition(jsonTestCaseBasic, condition, expectedId, unexpectedId)
+	unsuitableData := fmt.Sprintf(`
+{
+	"name": "hello"
+}
+`)
+	err := verifyInvalidOperationError(unsuitableData, "name==hello", entitiesArrayNotFoundError())
 	if err != nil {
-		return err
+		t.Error(err)
 	}
-
-	// Filter by string field
-	condition = fmt.Sprintf("name%s%s", operator, object1Name)
-	err = verifyValueReturnedWithCondition(jsonTestCaseBasic, condition, expectedId, unexpectedId)
-	if err != nil {
-		return err
-	}
-	// Filter by nested string field
-	condition = fmt.Sprintf("division.name%s%s", operator, object1DivisionName)
-	err = verifyValueReturnedWithCondition(jsonTestCaseBasic, condition, expectedId, unexpectedId)
-	if err != nil {
-		return err
-	}
-
-	// Filter by int field
-	condition = fmt.Sprintf("version%s%s", operator, object1Version)
-	err = verifyValueReturnedWithCondition(jsonTestCaseBasic, condition, expectedId, unexpectedId)
-	if err != nil {
-		return err
-	}
-
-	return nil
 }
 
-func verifyValueReturnedWithCondition(data string, condition string, expectedValue string, notExpectedValue string) error {
+func TestFilterByConditionInvalidOperatorError(t *testing.T) {
+	type OperationErrorStruct struct {
+		condition string
+		fieldType string
+		operator  string
+	}
+
+	var (
+		jsonData = fmt.Sprintf(`
+[
+	{
+		"name": "Kyle",
+		"version": 3,
+		"a": {
+			"foo": ["bar"]
+		}
+	}
+]
+`)
+		testCase1  = OperationErrorStruct{"name<5", TypeString, lessThanOperator}
+		testCase2  = OperationErrorStruct{"name>5", TypeString, greaterThanOperator}
+		testCase3  = OperationErrorStruct{"name<=5", TypeString, lessThanEqualsOperator}
+		testCase4  = OperationErrorStruct{"name>=5", TypeString, greaterThanEqualsOperator}
+		testCase5  = OperationErrorStruct{"version contains 5", TypeInteger, containsOperator}
+		testCase6  = OperationErrorStruct{"a.foo==bar", TypeArray, equalsOperator}
+		testCase7  = OperationErrorStruct{"a.foo!=bar", TypeArray, notEqualsOperator}
+		testCase8  = OperationErrorStruct{"a.foo<bar", TypeArray, lessThanOperator}
+		testCase9  = OperationErrorStruct{"a.foo>bar", TypeArray, greaterThanOperator}
+		testCase10 = OperationErrorStruct{"a.foo>=bar", TypeArray, greaterThanEqualsOperator}
+		testCase11 = OperationErrorStruct{"a.foo<=bar", TypeArray, lessThanEqualsOperator}
+		testCases  = []OperationErrorStruct{testCase1, testCase2, testCase3, testCase4, testCase5, testCase6, testCase7, testCase8, testCase9, testCase10, testCase11}
+	)
+
+	for _, test := range testCases {
+		expectedError := invalidOperatorError(test.fieldType, test.operator)
+		if err := verifyInvalidOperationError(jsonData, test.condition, expectedError); err != nil {
+			t.Errorf("ERROR: %v", err)
+		}
+	}
+}
+
+func TestFilterByConditionContains(t *testing.T) {
+	var (
+		objId1 = uuid.NewString()
+		objId2 = uuid.NewString()
+		objId3 = uuid.NewString()
+
+		jsonData = fmt.Sprintf(`
+{
+	"entities": [
+		{
+			"id": "%s",
+			"address": "Eindhoven, The Netherlands.",
+			"letters": ["x", "y", "z"]
+		},
+		{
+			"id": "%s",
+			"address": "La Paz, Bolivia.",
+			"outerKey": {
+				"letters": ["f", "g"],
+				"carInfo": {
+					"carReg": "1206-DN-63"
+				}
+			}
+		},
+		{
+			"id": "%s",
+			"address": "Rotterdam, The Netherlands.",
+			"outerKey": { 
+				"innerKey": {
+					"letters": ["a", "b", "c"]
+				},
+				"carInfo": {
+					"carReg": "2912-D-98"
+				}
+			}
+		}
+	]
+}
+`, objId1, objId2, objId3)
+
+		testCase1 = OperatorTestCaseStruct{"address contains Eindhoven", []string{objId1}, []string{objId2, objId3}}
+		testCase2 = OperatorTestCaseStruct{"address contains La Paz", []string{objId2}, []string{objId1, objId3}}
+		testCase3 = OperatorTestCaseStruct{"address contains Nether", []string{objId1, objId3}, []string{objId2}}
+		testCase4 = OperatorTestCaseStruct{"letters contains z", []string{objId1}, []string{objId3, objId2}}
+		testCase5 = OperatorTestCaseStruct{"outerKey.letters contains g", []string{objId2}, []string{objId3, objId1}}
+		testCase6 = OperatorTestCaseStruct{"outerKey.carInfo.carReg contains DN", []string{objId2}, []string{objId3, objId1}}
+		testCase7 = OperatorTestCaseStruct{"outerKey.innerKey.letters contains b", []string{objId3}, []string{objId1, objId2}}
+		testCase8 = OperatorTestCaseStruct{"outerKey.carInfo.carReg contains 98", []string{objId3}, []string{objId1, objId2}}
+		testCases = []OperatorTestCaseStruct{
+			testCase1,
+			testCase2,
+			testCase3,
+			testCase4,
+			testCase5,
+			testCase6,
+			testCase7,
+			testCase8,
+		}
+	)
+
+	for _, test := range testCases {
+		err := verifyValueReturnedWithCondition(jsonData, test.condition, test.expectedValues, test.notExpectedValues)
+		if err != nil {
+			t.Errorf("ERROR: %v", err)
+		}
+	}
+}
+
+func TestFilterByConditionGreaterAndLessThanOperations(t *testing.T) {
+	var (
+		objId1 = uuid.NewString()
+		objId2 = uuid.NewString()
+		objId3 = uuid.NewString()
+		objId4 = uuid.NewString()
+
+		jsonData = fmt.Sprintf(`
+[
+	{
+		"id": "%s",
+		"version": 1
+	},
+	{
+		"id": "%s",
+		"version": 2
+	},
+	{
+		"id": "%s",
+		"version": 3
+	},
+	{
+		"id": "%s",
+		"version": 0
+	}
+]
+`, objId1, objId2, objId3, objId4)
+
+		testCase1 = OperatorTestCaseStruct{"version<2", []string{objId1, objId4}, []string{objId2, objId3}}
+		testCase2 = OperatorTestCaseStruct{"version<=2", []string{objId1, objId2, objId4}, []string{objId3}}
+		testCase3 = OperatorTestCaseStruct{"version>2", []string{objId3}, []string{objId1, objId2}}
+		testCase4 = OperatorTestCaseStruct{"version>=2", []string{objId2, objId3}, []string{objId1}}
+		testCase5 = OperatorTestCaseStruct{"version<10", []string{objId1, objId2, objId3, objId4}, []string{}}
+		testCases = []OperatorTestCaseStruct{testCase1, testCase2, testCase3, testCase4, testCase5}
+	)
+
+	for _, test := range testCases {
+		err := verifyValueReturnedWithCondition(jsonData, test.condition, test.expectedValues, test.notExpectedValues)
+		if err != nil {
+			t.Errorf("ERROR: %v", err)
+		}
+	}
+}
+
+func TestFilterByConditionEquals(t *testing.T) {
+	var (
+		object1Id = uuid.NewString()
+		object2Id = uuid.NewString()
+		jsonData  = fmt.Sprintf(`
+[
+	{
+		"id": "%s",
+		"name": "Charlie C",
+		"active": true,
+		"division": {
+			"name": "Home"
+		},
+		"version": 3,
+		"skills": ["Sonic Speed", "Invisibility"]
+	},
+	{
+		"id": "%s",
+		"name": "Darragh McD",
+		"active": false,
+		"division": {
+			"name": "New"
+		},
+		"version": 6,
+		"skills": ["Sonic Speed", "Invisibility"]
+	}
+]`, object1Id, object2Id)
+
+		testCase1 = OperatorTestCaseStruct{"active==true", []string{object1Id}, []string{object2Id}}
+		testCase2 = OperatorTestCaseStruct{"name==Charlie C", []string{object1Id}, []string{object2Id}}
+		testCase3 = OperatorTestCaseStruct{"division.name==Home", []string{object1Id}, []string{object2Id}}
+		testCase4 = OperatorTestCaseStruct{"version==3", []string{object1Id}, []string{object2Id}}
+		testCase5 = OperatorTestCaseStruct{"active!=true", []string{object2Id}, []string{object1Id}}
+		testCase6 = OperatorTestCaseStruct{"name!=Charlie C", []string{object2Id}, []string{object1Id}}
+		testCase7 = OperatorTestCaseStruct{"division.name!=Home", []string{object2Id}, []string{object1Id}}
+		testCase8 = OperatorTestCaseStruct{"version!=3", []string{object2Id}, []string{object1Id}}
+		testCase9 = OperatorTestCaseStruct{"name==Darragh McD", []string{object2Id}, []string{object1Id}}
+		testCases = []OperatorTestCaseStruct{testCase1, testCase2, testCase3, testCase4, testCase5, testCase6, testCase7, testCase8, testCase9}
+	)
+
+	for _, test := range testCases {
+		if err := verifyValueReturnedWithCondition(jsonData, test.condition, test.expectedValues, test.notExpectedValues); err != nil {
+			t.Errorf("ERROR: %v", err)
+		}
+	}
+}
+
+func TestFilterByConditionNestedJson(t *testing.T) {
+	var (
+		object1Id = uuid.NewString()
+		object2Id = uuid.NewString()
+		object3Id = uuid.NewString()
+		data      = fmt.Sprintf(`
+{
+	"entities": [
+		{
+			"id": "%s",
+			"name": "Alpha",
+			"a": {
+				"b": "Beta",
+				"c": {
+					"d": {
+						"f": "Gamma",
+						"g": ["Kappa"]
+					},
+					"e": 0
+				}
+			}
+		}, 
+		{
+			"id": "%s",
+			"name": "Zeta",
+			"a": {
+				"b": "Theta",
+				"c": {
+					"d": {
+						"f": "Epsilon",
+						"g": ["Iota"]
+					},
+					"e": 101
+				}
+			}
+		},
+		{
+			"id": "%s",
+			"name": "Beta",
+			"a": {
+				"b": "Iota",
+				"c": {
+					"d": {
+						"f": "Theta",
+						"g": ["Sigma", "Iota"]
+					},
+					"e": 43
+				}
+			}
+		}
+	]
+}
+`, object1Id, object2Id, object3Id)
+		testCase1 = OperatorTestCaseStruct{"a.c.d.f==Gamma", []string{object1Id}, []string{object2Id}}
+		testCase2 = OperatorTestCaseStruct{"a.c.d.f==Epsilon", []string{object2Id}, []string{object1Id}}
+		testCase3 = OperatorTestCaseStruct{"a.c.d.g contains Kappa", []string{object1Id}, []string{object2Id}}
+		testCase4 = OperatorTestCaseStruct{"a.c.d.g contains Iota", []string{object2Id, object3Id}, []string{object1Id}}
+		testCase5 = OperatorTestCaseStruct{"a.c.e<=0", []string{object1Id}, []string{object2Id}}
+		testCase6 = OperatorTestCaseStruct{"a.c.e>100", []string{object2Id}, []string{object1Id}}
+		testCase7 = OperatorTestCaseStruct{"a.c.e<100", []string{object3Id, object1Id}, []string{object2Id}}
+		testCase8 = OperatorTestCaseStruct{"a.b contains eta", []string{object1Id, object2Id}, []string{object3Id}}
+		testCases = []OperatorTestCaseStruct{testCase1, testCase2, testCase3, testCase4, testCase5, testCase6, testCase7, testCase8}
+	)
+
+	for _, test := range testCases {
+		if err := verifyValueReturnedWithCondition(data, test.condition, test.expectedValues, test.notExpectedValues); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func verifyInvalidOperationError(jsonData string, condition string, expectedError error) error {
+	expectedErrorStr := fmt.Sprintf("%v", expectedError)
+	err := verifyValueReturnedWithCondition(jsonData, condition, []string{}, []string{})
+	if err != nil {
+		if fmt.Sprintf("%v", err) != expectedErrorStr {
+			return fmt.Errorf("expected error: %s, got: %v", expectedErrorStr, err)
+		} else {
+			return nil
+		}
+	} else {
+		return fmt.Errorf("expected to receive error %s, got %v", expectedErrorStr, err)
+	}
+}
+
+func verifyValueReturnedWithCondition(data string, condition string, expectedValues []string, notExpectedValues []string) error {
 	result, err := FilterByCondition(data, condition)
 	if err != nil {
 		return err
 	}
-	if !strings.Contains(result, expectedValue) {
-		return fmt.Errorf("wrong json object returned. Expected to find value: '%s' with condition '%s'\nReceived:\n%s\n", expectedValue, condition, result)
+	for _, expectedValue := range expectedValues {
+		if !strings.Contains(result, expectedValue) {
+			return fmt.Errorf("wrong json object returned. Expected to find value: '%s' with condition '%s'\nReceived:\n%s\n", expectedValue, condition, result)
+		}
 	}
-	if strings.Contains(result, notExpectedValue) {
-		return fmt.Errorf("wrong json object returned. Did not expect to find value: '%s' with condition '%s'\nReceived:\n%s\n", notExpectedValue, condition, result)
+	for _, notExpectedValue := range notExpectedValues {
+		if strings.Contains(result, notExpectedValue) {
+			return fmt.Errorf("wrong json object returned. Did not expect to find value: '%s' with condition '%s'\nReceived:\n%s\n", notExpectedValue, condition, result)
+		}
 	}
 	return nil
-}
-
-func operatorIsSuitableForBoolFields(operator string) bool {
-	return operator == equalsOperator || operator == notEqualsOperator
-}
-
-func operatorIsSuitableForStringFields(operator string) bool {
-	return operator == equalsOperator || operator == notEqualsOperator || operator == containsOperator
-}
-
-func operatorIsSuitableForIntegers(operator string) bool {
-	return operator != containsOperator
 }
