3ab8aff992d0fbdb8877464de711ac141a10c70a
HemanthDogiparthi12 <127493303+HemanthDogiparthi12@users.noreply.github.com>
2023-07-28 11:08:32 +0100

    Update System Text to Latest version (#629)

--- resources/sdk/pureclouddotnet/scripts/publishNuget.sh
+++ resources/sdk/pureclouddotnet/scripts/publishNuget.sh
@@ -44,7 +44,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>'\
 '			<dependency id="Newtonsoft.Json" version="11.0.2" />'\
 '			<dependency id="RestSharp" version="110.2.0" />'\
 '			<dependency id="ini-parser" version="2.5.2" />'\
-'			<dependency id="System.Text.Json" version="6.0.0" />'\
+'			<dependency id="System.Text.Json" version="7.0.2" />'\
 '			<dependency id="System.Text.Encodings.Web" version="7.0.0" />'\
 '			<dependency id="System.Threading.Tasks.Extensions" version="4.5.4" />'\
 '		</group>'\
@@ -52,7 +52,7 @@ echo '<?xml version="1.0" encoding="utf-8"?>'\
 '			<dependency id="Newtonsoft.Json" version="11.0.2" />'\
 '			<dependency id="RestSharp" version="110.2.0" />'\
 '			<dependency id="ini-parser" version="2.5.2" />'\
-'			<dependency id="System.Text.Json" version="6.0.0" />'\
+'			<dependency id="System.Text.Json" version="7.0.2" />'\
 '			<dependency id="System.Text.Encodings.Web" version="7.0.0" />'\
 '			<dependency id="System.Threading.Tasks.Extensions" version="4.5.4" />'\
 '		</group>'\
