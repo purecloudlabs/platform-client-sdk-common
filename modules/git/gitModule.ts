6761ba0619e01a013ac5a36e10ac21b3c5b0142c
HemanthDogiparthi12 <hemanth.dogiparthi@genesys.com>
2023-09-06 14:35:24 +0100

    Review Comment Updates

--- modules/git/gitModule.ts
+++ modules/git/gitModule.ts
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 import fs from 'fs-extra';
-import { spawn ,SpawnOptions } from 'child_process';
+import { spawn, SpawnOptions } from 'child_process';
 import Q from 'q';
 import Logger from '../log/logger';
 
@@ -14,126 +14,109 @@ export default class Git {
 
 	clone(repo: string, branch: string, target: string): Promise<string> {
 		return new Promise<string>((resolve, reject) => {
-		  try {
-			// Skip cloning if no repo was provided
-			if (!repo || repo === '') {
-			  log.info('Repo was undefined. Skipping clone.');
-			  // Ensure repo dir exists anyway; we still need to build the SDK
-			  fs.ensureDirSync(target);
-			  resolve('');
-			  return;
+			try {
+				// Skip cloning if no repo was provided
+				if (!repo || repo === '') {
+					log.info('Repo was undefined. Skipping clone.');
+					// Ensure repo dir exists anyway; we still need to build the SDK
+					fs.ensureDirSync(target);
+					resolve('');
+					return;
+				}
+
+				repo = this.injectAuthToken(repo, this.authToken);
+				let args = [];
+				args.push('clone');
+				args.push('--quiet');
+				args.push('--progress');
+				if (branch) {
+					args.push('--branch');
+					args.push(branch);
+				}
+				args.push('--depth');
+				args.push('1');
+				args.push(repo);
+				args.push(target);
+
+
+
+				this.spawnProcess(args, undefined)
+					.then(() => {
+						resolve('Cloning successful.');
+					})
+					.catch((err) => {
+						reject(err);
+					});
+			} catch (err) {
+				reject(err);
 			}
-	
-			repo = this.injectAuthToken(repo, this.authToken);
-			let args = [];
-			args.push('clone');
-			args.push('--quiet');
-			args.push('--progress');
-			if (branch) {
-			  args.push('--branch');
-			  args.push(branch);
-			}
-			args.push('--depth');
-			args.push('1');
-			args.push(repo);
-			args.push(target);
-	
-			
-			
-			this.spawnProcess(args, undefined)
-          .then(() => {
-            resolve('Cloning successful.');
-          })
-          .catch((err) => {
-            reject(err);
-          });
-      } catch (err) {
-        reject(err);
-      }
 		});
-	  }
+	}
 
-saveChanges(repo: string, localDir: string, message: string) : Promise<string> {
-	return new Promise<string>((resolve, reject)  => {
+	saveChanges(repo: string, localDir: string, message: string): Promise<string> {
+		return new Promise<string>((resolve, reject) => {
 
-	try {
-		// Skip commit if no repo was provided
-		if (!repo || repo === '') {
-			log.info('Repo was undefined. Skipping commit/push.');
-			resolve("");
+			try {
+				// Skip commit if no repo was provided
+				if (!repo || repo === '') {
+					log.info('Repo was undefined. Skipping commit/push.');
+					resolve("");
+				}
+
+				repo = this.injectAuthToken(repo, this.authToken);
+				this.spawnProcess(['add', '-A'], localDir)
+					.then(() => {
+						let commitArgs = ['commit', '-m', message ? message : 'automated commit'];
+						return this.spawnProcess(commitArgs, localDir);
+					})
+					.then(() => {
+						return this.spawnProcess(['push', `--repo=${repo}`], localDir);
+					})
+					.then(() => resolve(""))
+					.catch((err) => reject(err));
+			} catch (err) {
+				reject(err);
+			}
+		});
+	}
+
+
+	injectAuthToken(repo, authToken) {
+		if (authToken) {
+			if (repo.startsWith('git://')) repo = `git://${authToken}@${repo.substring(6)}`;
+			else if (repo.startsWith('http://')) repo = `http://${authToken}@${repo.substring(7)}`;
+			else if (repo.startsWith('https://')) repo = `https://${authToken}@${repo.substring(8)}`;
+			else throw new Error("Can't figure out where to put the auth token! URL is not GIT, HTTP, or HTTPS!");
 		}
-
-		repo = this.injectAuthToken(repo, this.authToken);
-		this.spawnProcess(['add', '-A'], localDir)
-			.then(() => {
-				let commitArgs = ['commit', '-m', message ? message : 'automated commit'];
-				return this.spawnProcess(commitArgs, localDir);
-			})
-			.then(() => {
-				return this.spawnProcess(['push', `--repo=${repo}`], localDir);
-			})
-			.then(() => resolve(""))
-			.catch((err) => reject(err));
-	} catch (err) {
-	reject(err);
+		log.debug(`repo: ${repo}`);
+		return repo;
 	}
-});
-}
 
-
-injectAuthToken(repo, authToken) {
-	if (authToken) {
-		if (repo.startsWith('git://')) repo = `git://${authToken}@${repo.substring(6)}`;
-		else if (repo.startsWith('http://')) repo = `http://${authToken}@${repo.substring(7)}`;
-		else if (repo.startsWith('https://')) repo = `https://${authToken}@${repo.substring(8)}`;
-		else throw new Error("Can't figure out where to put the auth token! URL is not GIT, HTTP, or HTTPS!");
+	private spawnProcess(args: string[], localDir: string | undefined): Promise<void> {
+		return new Promise<void>((resolve, reject) => {
+			try {
+				log.debug(`Spawn: git ${args.join(' ')}`);
+				let options: SpawnOptions = {
+					stdio: 'inherit',
+					cwd: undefined,
+				};
+				if (localDir) {
+					log.debug(`Using cwd: ${localDir}`);
+					options.cwd = localDir;
+				}
+				let cmd = spawn('git', args, options);
+				cmd.on('error', (err) => {
+					log.error(`Git operation failed: ${err.message}`);
+					reject(err);
+				});
+				cmd.on('close', (code) => {
+					log.info(`Git operation exited with code ${code}`);
+					if (code === 0) resolve();
+					else reject(new Error(`Git operation exited with code ${code}`));
+				});
+			} catch (err) {
+				reject(err);
+			}
+		});
 	}
-	log.debug(`repo: ${repo}`);
-	return repo;
 }
-
-
-
-
-
-
-private spawnProcess(args: string[], localDir: string | undefined): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
-      try {
-        log.debug(`Spawn: git ${args.join(' ')}`);
-        let options: SpawnOptions = {
-          stdio: 'inherit',
-          cwd: undefined,
-        };
-        if (localDir) {
-          log.debug(`Using cwd: ${localDir}`);
-          options.cwd = localDir;
-        }
-        let cmd = spawn('git', args, options);
-        cmd.on('error', (err) => {
-          log.error(`Git operation failed: ${err.message}`);
-          reject(err);
-        });
-        cmd.on('close', (code) => {
-          log.info(`Git operation exited with code ${code}`);
-          if (code === 0) resolve();
-          else reject(new Error(`Git operation exited with code ${code}`));
-        });
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
--- modules/log/logger.ts
+++ modules/log/logger.ts
@@ -1,27 +1,26 @@
 import _ from 'lodash';
 import Winston from 'winston';
-//const Winston = require('winston');
 
 
 export default class Logger {
 
-	 public log: any;
+	public log: any;
 
-	 private defaultWidth: number = 0;
+	private defaultWidth: number = 0;
 
-  constructor() {
-    this.log = new Winston.Logger({
-      transports: [
-        new Winston.transports.Console({
-          level: 'silly',
-          handleExceptions: true,
-          json: false,
-          colorize: false,
-        }),
-      ],
-    });
-  }
-  
+	constructor() {
+		this.log = new Winston.Logger({
+			transports: [
+				new Winston.transports.Console({
+					level: 'silly',
+					handleExceptions: true,
+					json: false,
+					colorize: false,
+				}),
+			],
+		});
+	}
+
 	public setLogLevel(level: string): void {
 		level = checkLevel(level);
 		this.log.transports.console.level = level;
@@ -34,35 +33,35 @@ export default class Logger {
 	};
 
 
-	public silly(msg: string){
+	public silly(msg: string) {
 		this.log.silly(msg);
 	}
 
-	public debug(msg: string){
+	public debug(msg: string) {
 		this.log.debug(msg);
 	}
 
-	public verbose(msg: string){
+	public verbose(msg: string) {
 		this.log.verbose(msg);
 	}
 
-	public info(msg: string){
+	public info(msg: string) {
 		this.log.info(msg);
 	}
 
-	public error(msg: string){
+	public error(msg: string) {
 		this.log.error(msg);
 	}
 
-	public profile(msg: string){
+	public profile(msg: string) {
 		this.log.profile(msg);
 	}
 
-	public warn(msg: string){
+	public warn(msg: string) {
 		this.log.warn(msg);
 	}
 
-	public writeBoxedLine(string :string, width: number , padchar : string, level: string) {
+	public writeBoxedLine(string: string, width: number, padchar: string, level: string) {
 		level = checkLevel(level);
 		if (!width) width = this.defaultWidth;
 		if (!padchar) padchar = ' ';
@@ -70,7 +69,7 @@ export default class Logger {
 		let words = string.split(' ');
 		let rows = [];
 		let c = 0;
-		_.forEach(words, function(word, index) {
+		_.forEach(words, function (word, index) {
 			if (!rows[c]) rows[c] = '';
 			if (rows[c].length + word.length + 1 > cWidth) {
 				c++;
@@ -78,10 +77,10 @@ export default class Logger {
 			}
 			rows[c] += word + ' ';
 		});
-	
+
 		// Lodash messes with this/self. self is set to the Builder object for some reason.
 		let logObject = this.log;
-		_.forEach(rows, function(row, index) {
+		_.forEach(rows, function (row, index) {
 			logObject.log(level, '║ ' + pad(row.trimRight(), cWidth, padchar) + ' ║');
 		});
 	};
@@ -92,113 +91,29 @@ export default class Logger {
 		if (!width) width = this.defaultWidth;
 		this.log.log(level, '╔' + pad('', width - 2, '═') + '╗');
 	};
-	
+
 	public writeBoxSeparator(width, level) {
 		level = checkLevel(level);
 		if (!width) width = this.defaultWidth;
 		this.log.log(level, '╟' + pad('', width - 2, '─') + '╢');
 	};
-	
+
 	public writeBoxBottom(width, level) {
 		level = checkLevel(level);
 		if (!width) width = this.defaultWidth;
 		this.log.log(level, '╚' + pad('', width - 2, '═') + '╝');
 	};
-	
+
 	public writeBox(string, width = 0, level = "info") {
 		// default boxes to info
 		level = level ? level : 'info';
 
-		if (width == 0) width = string.length < this.defaultWidth ? this.defaultWidth : string.length + 5; 
+		if (width == 0) width = string.length < this.defaultWidth ? this.defaultWidth : string.length + 5;
 		this.writeBoxTop(width, level);
 		this.writeBoxedLine(string, width, null, level);
 		this.writeBoxBottom(width, level);
 	};
-
-	// Logger.prototype.silly = function(msg) {
-	// 	this.log.silly(msg);
-	// };
-	
-
-	
 }
-
-
-
-
-//function Logger() {}
-
-// Logger.prototype.log = new Winston.Logger({
-// 	transports: [
-// 		new Winston.transports.Console({
-// 			level: 'silly',
-// 			handleExceptions: true,
-// 			json: false,
-// 			colorize: false
-// 		})
-// 	]
-// });
-
-// Logger.prototype.setLogLevel = function(level) {
-// 	level = checkLevel(level);
-// 	this.log.transports.console.level = level;
-// 	this.log.info(`Log level set to ${level}`);
-// };
-
-// Logger.prototype.setUseColor = function(useColor) {
-// 	this.log.transports.console.colorize = useColor === true;
-// 	this.log.info(`Logger will use color: ${useColor}`);
-// };
-
-// // Passthrough functions
-// Logger.prototype.silly = function(msg) {
-// 	this.log.silly(msg);
-// };
-// Logger.prototype.debug = function(msg) {
-// 	this.log.debug(msg);
-// };
-// Logger.prototype.verbose = function(msg) {
-// 	this.log.verbose(msg);
-// };
-// Logger.prototype.info = function(msg) {
-// 	this.log.info(msg);
-// };
-// Logger.prototype.warn = function(msg) {
-// 	this.log.warn(msg);
-// };
-// Logger.prototype.error = function(msg) {
-// 	this.log.error(msg);
-// };
-
-// Logger.prototype.profile = function(msg) {
-// 	this.log.profile(msg);
-// };
-
-// Logger.prototype.writeBoxedLine = function(string, width, padchar, level) {
-// 	level = checkLevel(level);
-// 	if (!width) width = this.defaultWidth;
-// 	if (!padchar) padchar = ' ';
-// 	let cWidth = width - 4;
-// 	let words = string.split(' ');
-// 	let rows = [];
-// 	let c = 0;
-// 	_.forEach(words, function(word, index) {
-// 		if (!rows[c]) rows[c] = '';
-// 		if (rows[c].length + word.length + 1 > cWidth) {
-// 			c++;
-// 			rows[c] = '';
-// 		}
-// 		rows[c] += word + ' ';
-// 	});
-
-// 	// Lodash messes with this/self. self is set to the Builder object for some reason.
-// 	let logObject = this.log;
-// 	_.forEach(rows, function(row, index) {
-// 		logObject.log(level, '║ ' + pad(row.trimRight(), cWidth, padchar) + ' ║');
-// 	});
-// };
-
-
 
 function pad(value, length, padchar) {
 	return value.toString().length < length ? pad(value + padchar, length, padchar) : value;
@@ -207,5 +122,3 @@ function pad(value, length, padchar) {
 function checkLevel(level) {
 	return level ? level : 'debug';
 }
-
-//module.exports = new Logger();
