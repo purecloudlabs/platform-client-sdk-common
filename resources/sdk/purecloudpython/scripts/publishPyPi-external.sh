6548a3516806a92c49afee2d54b67cf6c103656d
John Carnell <john.carnell@genesys.com>
2024-02-20 12:19:52 -0500

    NO-JIRA-PYPI: Split the pub scripts into an internal vs external set of scripts.  The external scripts use the authentication mechanism from PYPI.  The internal pub script uses jfrog and its old authentication mechanism

---
+++ resources/sdk/purecloudpython/scripts/publishPyPi-external.sh
@@ -0,0 +1,31 @@
+INDEX_SERVER=$1
+IS_NEW_RELEASE=$2
+VERSION=$3
+
+echo "INDEX_SERVER=$INDEX_SERVER"
+echo "IS_NEW_RELEASE=$IS_NEW_RELEASE"
+echo "VERSION=$VERSION"
+
+if [ ! "$IS_NEW_RELEASE" = "true" ]
+then
+	echo "Skipping PyPi deploy"
+	exit 0
+fi
+
+echo "Upgrading PIP"
+python3.6 -m pip install --upgrade --user pip
+
+echo "Installing Twine and Wheel"
+python3.6 -m pip install twine wheel setuptools --user --upgrade
+
+echo "Creating the distribution package"
+python3.6 setup.py sdist bdist_wheel
+
+echo "Running twine check"
+python3.6 -m twine check dist/*
+
+echo "uploading to the pypi test server"
+python3.6 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/* -u __token__ -p $DEVTOOLING_SDK_PYPI_TEST
+
+echo "uploading to the pypi prod server"
+python3.6 -m twine upload --verbose --repository-url https://upload.pypi.org/legacy/ dist/* -u __token__ -p $DEVTOOLING_SDK_PYPI_PROD
