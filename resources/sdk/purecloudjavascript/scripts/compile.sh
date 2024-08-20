252089b26f38f3fb67c0d186c761e90a71feed0d
Tim Smith <tim.j.smith@genesys.com>
2024-08-20 10:23:55 -0600

    update build commands

--- resources/sdk/purecloudjavascript/scripts/compile.sh
+++ resources/sdk/purecloudjavascript/scripts/compile.sh
@@ -12,34 +12,28 @@ echo "PACKAGE_NAME=$PACKAGE_NAME"
 
 cd $BUILD_DIR
 
-echo "Installing npm global modules..."
-npm i -g rollup@"^2.67.2"
-npm i -g uglify-es@"^3.3.9"
-npm i -g browserify@"^17.0.0"
-
 echo "Installing dependencies..."
 npm i
 
 # Build cjs/node
 echo "Executing rollup (cjs/node)..."
-rollup -c rollup-cjs-for-node.config.js || { echo "Rollup (cjs/node) failed"; exit 1; }
+./node_modules/rollup/dist/bin/rollup -c rollup-cjs-for-node.config.js || { echo "Rollup (cjs/node) failed"; exit 1; }
 
 # Build cjs/browserify
 echo "Executing rollup (cjs/browserify)..."
-rollup -c rollup-cjs-for-browserify.config.js || { echo "Rollup (cjs/browserify) failed"; exit 1; }
+./node_modules/rollup/dist/bin/rollup -c rollup-cjs-for-browserify.config.js || { echo "Rollup (cjs/browserify) failed"; exit 1; }
 
 echo "Browserifying..."
-browserify -r "$BUILD_DIR/dist/web-cjs/bundle.js:${MODULE_NAME}" > "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.js" || { echo "Browserify failed"; exit 1; }
+./node_modules/browserify/bin/cmd.js -r "$BUILD_DIR/dist/web-cjs/bundle.js:${MODULE_NAME}" > "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.js" || { echo "Browserify failed"; exit 1; }
 
 echo "Minifying...."
-uglifyjs "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.js" --compress --mangle --output "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.min.js" || { echo "Minify failed (cjs/browserify)"; exit 1; }
+./node_modules/uglify-es/bin/uglifyjs "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.js" --compress --mangle --output "$BUILD_DIR/dist/web-cjs/$PACKAGE_NAME.min.js" || { echo "Minify failed (cjs/browserify)"; exit 1; }
 
 # Build amd
 echo "Executing rollup (amd)..."
-rollup -c rollup-amd.config.js || { echo "Rollup (amd) failed"; exit 1; }
+./node_modules/rollup/dist/bin/rollup -c rollup-amd.config.js || { echo "Rollup (amd) failed"; exit 1; }
 
 echo "Minifying...."
-uglifyjs "$BUILD_DIR/dist/web-amd/$PACKAGE_NAME.js" --compress --mangle --output "$BUILD_DIR/dist/web-amd/$PACKAGE_NAME.min.js" || { echo "Minify failed (amd)"; exit 1; }
-
+./node_modules/uglify-es/bin/uglifyjs "$BUILD_DIR/dist/web-amd/$PACKAGE_NAME.js" --compress --mangle --output "$BUILD_DIR/dist/web-amd/$PACKAGE_NAME.min.js" || { echo "Minify failed (amd)"; exit 1; }
 
 echo "Compile script complete"
