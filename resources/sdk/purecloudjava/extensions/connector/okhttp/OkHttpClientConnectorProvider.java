7439d392e3d771d4ef2713d71b3a491faddcd632
kavinbalagen <kavin_arasu.balasubramanian@genesys.com>
2025-04-04 16:01:02 -0400

    restore OkHttpClient

--- resources/sdk/purecloudjava/extensions/connector/okhttp/OkHttpClientConnectorProvider.java
+++ resources/sdk/purecloudjava/extensions/connector/okhttp/OkHttpClientConnectorProvider.java
@@ -7,23 +7,11 @@ import com.mypurecloud.sdk.v2.connector.ApiClientConnectorProvider;
 import okhttp3.Dispatcher;
 import okhttp3.OkHttpClient;
 
-import java.io.FileInputStream;
 import java.net.Proxy;
-import java.security.KeyStore;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import javax.net.ssl.KeyManagerFactory;
-import javax.net.ssl.SSLContext;
-
-import javax.net.ssl.TrustManagerFactory;
-import javax.net.ssl.X509TrustManager;
-
-
-
 public class OkHttpClientConnectorProvider implements ApiClientConnectorProvider {
-
-
     @Override
     public ApiClientConnector create(ApiClientConnectorProperties properties) {
         OkHttpClient.Builder builder = new OkHttpClient().newBuilder();
@@ -44,39 +32,7 @@ public class OkHttpClientConnectorProvider implements ApiClientConnectorProvider
         if (executorService != null) {
             builder.dispatcher(new Dispatcher(executorService));
         }
-        String keyStorePath = properties.getProperty(ApiClientConnectorProperty.KEYSTORE_PATH, String.class, null);
-        String keyStorePassword = properties.getProperty(ApiClientConnectorProperty.KEYSTORE_PASSWORD, String.class, null);
-        String trustStorePath = properties.getProperty(ApiClientConnectorProperty.TRUSTSTORE_PATH, String.class, null);
-        String trustStorePassword = properties.getProperty(ApiClientConnectorProperty.TRUSTSTORE_PASSWORD, String.class, null);
-        
-        try{
-            if (keyStorePath != null && keyStorePassword != null) {
-                        
-            KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-            KeyStore keyStore = KeyStore.getInstance("PKCS12");
-            try (FileInputStream keyStoreFileInputStream = new FileInputStream(keyStorePath)) {
-                keyStore.load(keyStoreFileInputStream, keyStorePassword.toCharArray());
-                keyManagerFactory.init(keyStore, keyStorePassword.toCharArray());
-            }
-          
-            TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
-            KeyStore trustStore = KeyStore.getInstance("PKCS12");
-            try (FileInputStream trustStoreFileInputStream = new FileInputStream(trustStorePath)) {
-                trustStore.load(trustStoreFileInputStream, trustStorePassword.toCharArray());
-                trustManagerFactory.init(trustStore);
-            }
-            SSLContext sslContext = SSLContext.getInstance("TLSv3");
-            sslContext.init(keyManagerFactory.getKeyManagers(), trustManagerFactory.getTrustManagers(), null);
 
-            builder.sslSocketFactory(sslContext.getSocketFactory(), (X509TrustManager)trustManagerFactory.getTrustManagers()[0]);
-            }
-    
-        }
-            catch (Exception e) {
-            throw new RuntimeException(e);
-
-        }
-         
         return new OkHttpClientConnector(builder.build());
     }
 }
