ad41288d9616ca847c98da41533735fa63d960e8
themooks <jerome.saint-marc@genesys.com>
2025-09-10 12:03:05 +0200

    Jenkins V3 migration test

--- resources/scripts/connect-proxy.sh
+++ resources/scripts/connect-proxy.sh
@@ -2,14 +2,14 @@
 start_proxy() {
 env=$1
 set +e
-if ! command -v pm2 &> /dev/null
+if ! command -v npm run run-pm2 -- &> /dev/null
 then
     echo "pm2 could not be found. Installing it now..."
-    npm install pm2 -g
+    npm install pm2
 fi
 # Check if the app is already running
 # Start the app
-PURECLOUD_ENV=$env pm2 start ./resources/scripts/pm2.config.cjs
+PURECLOUD_ENV=$env npm run run-pm2 -- start ./resources/scripts/pm2.config.cjs
 echo "App started."
 
 set -e
@@ -18,20 +18,20 @@ set -e
 
 stop_proxy() {
     set +e
-    if ! command -v pm2 &> /dev/null; then
+    if ! command -v npm run run-pm2 -- &> /dev/null; then
         echo "pm2 is not installed. Exiting..."
         exit 0
     fi
 
-    if pm2 list | grep -q "proxy-server"; then
-        pm2 delete proxy-server
+    if npm run run-pm2 -- list | grep -q "proxy-server"; then
+        npm run run-pm2 -- delete proxy-server
         echo "Process proxy-server is stopped."
     else
         echo "process proxy-server is not running."
     fi
 
-    if pm2 list | grep -q "gateway-server"; then
-            pm2 delete gateway-server
+    if npm run run-pm2 -- list | grep -q "gateway-server"; then
+            npm run run-pm2 -- delete gateway-server
             echo "Process gateway-server is stopped."
         else
             echo "process gateway-server is not running."
